ec67343127b3f0f652c5afc0c9ea8dcc70457f4f
Unknown <brian_poblete@dlsu.edu.ph>
2018-04-09 00:26:06 +0800

    DoctorModule is now the driver for the doctor segment
    The driver for the old DC2 is now DesignChallenge2 again and DoctorModule is the driver for the actual doctor module.

---
+++ DesignChallenge1/src/DesignChallenge2.java
@@ -0,0 +1,34 @@
+
+public class DesignChallenge2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	
+        CalendarView cpv = new CalendarProgramView();
+        CalendarModel cpm = new CalendarProgramModel();
+        CalendarControl cpc = new CalendarProgramControl();
+        AgendaView apv = new AgendaProgramView(cpv);
+        DayView dpv = new DayProgramView(cpv);
+        WeekView wpv = new WeekProgramView(cpv);
+        cpv.attachController(cpc);
+        cpv.attachAgendaView(apv);
+        cpv.attachDayView(dpv);
+        cpv.attachWeekView(wpv);
+//        cpm.connectDatabase("com.mysql.jdbc.Driver","jdbc:mysql://192.168.43.217:3306/","superuser","sophia","SWDESPA");
+        cpm.connectDatabase("com.mysql.jdbc.Driver","jdbc:mysql://192.168.0.4:3306/","superuser1","12345","swdespa");
+        cpm.attachView(cpv);
+        cpc.attachModel(cpm);
+        cpm.attachObserver((ObserverView) apv);
+        cpm.attachObserver((ObserverView) dpv);
+        cpm.attachObserver((ObserverView) wpv);
+        cpv.initialize();
+        cpv.refreshView();
+        cpm.timeCheck();
+    	//com.mysql.jdbc.Driver
+    	//jdbc:mysql://127.0.0.1:3306/
+    	//root (username)
+    	//july (password)
+    	//SWDESPA (database)
+    }
+    
+}
--- DesignChallenge1/src/DoctorModule.java
+++ DesignChallenge1/src/DoctorModule.java
@@ -1,34 +1,13 @@
 
 public class DoctorModule {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
-    	
-        CalendarView cpv = new CalendarProgramView();
-        CalendarModel cpm = new CalendarProgramModel();
-        CalendarControl cpc = new CalendarProgramControl();
-        AgendaView apv = new AgendaProgramView(cpv);
-        DayView dpv = new DayProgramView(cpv);
-        WeekView wpv = new WeekProgramView(cpv);
-        cpv.attachController(cpc);
-        cpv.attachAgendaView(apv);
-        cpv.attachDayView(dpv);
-        cpv.attachWeekView(wpv);
-//        cpm.connectDatabase("com.mysql.jdbc.Driver","jdbc:mysql://192.168.43.217:3306/","superuser","sophia","SWDESPA");
-        cpm.connectDatabase("com.mysql.jdbc.Driver","jdbc:mysql://172.20.6.221:3306/","superuser1","12345","swdespa");
-        cpm.attachView(cpv);
-        cpc.attachModel(cpm);
-        cpm.attachObserver((ObserverView) apv);
-        cpm.attachObserver((ObserverView) dpv);
-        cpm.attachObserver((ObserverView) wpv);
-        cpv.initialize();
-        cpv.refreshView();
-        cpm.timeCheck();
-    	//com.mysql.jdbc.Driver
-    	//jdbc:mysql://127.0.0.1:3306/
-    	//root (username)
-    	//july (password)
-    	//SWDESPA (database)
-    }
-    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoctorMenuView dmv = new DoctorMenuView();
+		DoctorLoginView dlv = new DoctorLoginView(dmv);
+		DoctorSlotAdderView dsav = new DoctorSlotAdderView(dmv);
+		dmv.attachDoctorLoginView(dlv);
+		dmv.attachDoctorSlotAdderView(dsav);
+	}
+
 }
